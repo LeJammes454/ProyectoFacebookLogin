bd6b9cd779afb6ccaff6f132ae57dd8b7751ed81
LeJammes454 <98853831+LeJammes454@users.noreply.github.com>
2023-05-25 03:26:48 -0600

    v2 Mamalona

--- app/src/main/java/com/example/proyectofacelogin/MainActivity.kt
+++ app/src/main/java/com/example/proyectofacelogin/MainActivity.kt
@@ -1,79 +1,62 @@
 package com.example.proyectofacelogin
 import android.content.Intent
-import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.*
-import com.facebook.login.LoginManager
+import android.os.Bundle
+import com.example.proyectofacelogin.databinding.ActivityMainBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var callbackManager: CallbackManager
 
-    private val PERMISSIONS = listOf("pages_read_engagement", "pages_manage_posts")
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // Inicializar el CallbackManager
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         callbackManager = CallbackManager.Factory.create()
+        binding.loginButton.setPermissions("email")
 
-        // Configurar el botón de inicio de sesión de Facebook
-        val loginButton: LoginButton = findViewById(R.id.login_button)
-        loginButton.setPermissions(PERMISSIONS)
-        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-          
+        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                val accessToken = loginResult.accessToken
+
+                // Obtener datos del usuario
+                val request = GraphRequest.newMeRequest(accessToken) { jsonObject, response ->
+                    val name = jsonObject?.optString("name")
+                    val email = jsonObject?.optString("email")
+
+                    // Mostrar datos en la interfaz de usuario
+                    binding.txtName.text = name
+                    binding.txtEmail.text = email
+                }
+
+                val parameters = Bundle()
+                parameters.putString("fields", "name,email")
+                request.parameters = parameters
+                request.executeAsync()
+            }
 
             override fun onCancel() {
-                // El usuario canceló la solicitud de permisos
+                // El usuario canceló el inicio de sesión con Facebook
             }
 
             override fun onError(error: FacebookException) {
-                TODO("Not yet implemented")
+                // Ocurrió un error durante el inicio de sesión con Facebook
             }
-
-            override fun onSuccess(result: LoginResult) {
-                publishPostToPage()
-            }
-
         })
     }
 
-    // Sobrescribir el método onActivityResult para llamar a callbackManager.onActivityResult
-    // Esto es necesario para manejar las respuestas del inicio de sesión de Facebook
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager.onActivityResult(requestCode, resultCode, data)
     }
-
-    private fun publishPostToPage() {
-        val parameters = Bundle()
-        parameters.putString("message", "Texto de tu publicación")
-
-        val request = GraphRequest(
-            AccessToken.getCurrentAccessToken(),
-            "/{page-id}/feed",
-            parameters,
-            HttpMethod.POST,
-            object : GraphRequest.Callback {
-                override fun onCompleted(response: GraphResponse) {
-                    // Aquí puedes manejar la respuesta de la publicación
-                    val postId = response.getJSONObject()?.getString("id")
-                    if (postId != null) {
-                        // La publicación se realizó exitosamente
-                        Log.d("Post", "Publicación exitosa. ID: $postId")
-                    } else {
-                        // La publicación falló
-                        Log.d("Post", "La publicación falló")
-                    }
-                }
-            }
-        )
-
-        request.executeAsync()
-    }
-
-
 }
